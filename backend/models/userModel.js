c9f571896ed70958a73a9bc7aa63fdaf51ff4753
Anton Knutsson <cykelkorg95@gmail.com>
2024-06-07 15:25:03 +0200

    added role to users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,10 @@
-import Joi from 'joi'
+import Joi  from 'joi'
 
 const userSchema = Joi.object({
-    username: Joi.string().min(5).max(10).alphanum().required(),
+    username: Joi.string().min(3).max(15).alphanum().required(),
     password: Joi.string().min(8).required(),
-    email: Joi.string().email().required()
+    email: Joi.string().email().required(),
+    role: Joi.string().required()
 })
 
 export default userSchema;
